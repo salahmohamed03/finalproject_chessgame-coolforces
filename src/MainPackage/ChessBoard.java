1e853e60e4ad51e3cddd112179c9abd9c7186af3
salah mohamed <salah.ismael03@eng-st.cu.edu.eg>
2023-04-27 01:44:07 +0200

    game frame

--- src/MainPackage/ChessBoard.java
+++ src/MainPackage/ChessBoard.java
@@ -5,52 +5,40 @@ import java.awt.event.ActionEvent;
 
 public class ChessBoard extends JFrame{
     public JFrame board;
-    public JButton button;
+    private PieceIcons icon;
+    public JButton button,resign,draw, resign2,draw2;
     public String current;
+    Icon drag;
     public String previous;
     private boolean turn;
     private JPanel container;
     private JPanel ChessBoardPanel;
     public ChessBoard(){initialize();}
-
     public void initialize() {
-        PieceIcons icon = new PieceIcons();
+        icon = new PieceIcons();
         turn = false;
         initialize_board();
+        setButtons();
         draw_chessBoard();
-        get_button("E4").setIcon(new ImageIcon(icon.black_bishop));// test
-        get_button("E5").setIcon(new ImageIcon(icon.white_bishop));// test
+        initializePieces();
         //move_piece("E4","A8");
-        //set_backgrounds();
+        set_backgrounds();
         board.setVisible(true);// show the board
     }
     public void initialize_board(){
         board = new JFrame();
-        initialize_container();
+        container = new JPanel(null);
         board.add(container);
         board.setTitle("chess");
-        board.setSize(864,614);
+        board.setSize(870,650);
         board.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         board.setLocationRelativeTo(null);
         board.setResizable(false);
     }
-    private void initialize_container(){
-        container = new JPanel(null) {
-            @Override
-            protected void paintComponent(Graphics g) {
-                Graphics2D g2d = (Graphics2D) g;
-                int w = getWidth();
-                int h = getHeight();
-                GradientPaint gp = new GradientPaint(0, 0, Color.decode("#361624"), w, 0, Color.decode("#FF006E"));
-                g2d.setPaint(gp);
-                g2d.fillRect(0, 0, w, h);
-            }
-        };
-    }
     public void draw_chessBoard(){
         ChessBoardPanel = new JPanel(new GridLayout(8 , 8 , 0 , 0));
         container.add(ChessBoardPanel);
-        ChessBoardPanel.setBounds(40 ,40,480,480);
+        ChessBoardPanel.setBounds(70 ,70,478,478);
         JButton [][]pos = new JButton[8][8];
         for(int i = 0 ;i < 8; i++)
             for(int j = 0 ; j < 8; j++)
@@ -66,12 +54,55 @@ public class ChessBoard extends JFrame{
                 ChessBoardPanel.add(pos[i][j]);
             }
     }
-    private void set_backgrounds()
+    public void initializePieces(){
+        get_button("A2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("B2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("C2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("D2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("E2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("F2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("G2").setIcon(new ImageIcon(icon.white_pawn));
+        get_button("H2").setIcon(new ImageIcon(icon.white_pawn));
+    }
+    public JButton createButton(String name, String filename, int x , int y) {
+        JButton temp = new JButton(name);
+        temp.setFocusable(false);
+        temp.setFont(new Font("SpaceGrotesk",Font.PLAIN,20));
+        ImageIcon temp1 = new ImageIcon(filename);//src/Mat/Buttons/resignBtn.png
+        Image btnBG = temp1.getImage().getScaledInstance(66,30,Image.SCALE_SMOOTH);
+        temp.setIcon(new ImageIcon (btnBG));
+        temp.setHorizontalTextPosition(JButton.CENTER);
+        temp.setVerticalTextPosition(JButton.CENTER);
+        temp.setOpaque(false);
+        temp.setBorder(BorderFactory.createEmptyBorder());
+        temp.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("hello world");
+            }
+        });
+        temp.setBounds(x,y,66,30);
+        return temp;
+    }
+    public void setButtons()
     {
-        ImageIcon backgroundImage = new  ImageIcon("src/pieces/test.jpg");
+        resign = createButton("resign","src/Mat/Buttons/drawBtn.png",640,250);
+        draw = createButton("draw","src/Mat/Buttons/resignBtn.png",730,250);
+        resign2 = createButton("resign2","src/Mat/Buttons/drawBtn.png",640,340);
+        draw2 = createButton("draw2","src/Mat/Buttons/resignBtn.png",730,340);
+        container.add(resign);
+        container.add(draw);
+        container.add(resign2);
+        container.add(draw2);
+    }
+    private void set_backgrounds() {
+       // ImageIcon backgroundImage = new  ImageIcon("src/Mat/BackG/gameFields.png");
+        Image backgroundImage;
+        ImageIcon temp1 = new ImageIcon("src/Mat/BackG/rgameFields.png");
+       // backgroundImage = temp1.getImage().getScaledInstance(860,600,Image.SCALE_DEFAULT);
         JLabel background = new JLabel();
-        background.setIcon(backgroundImage);
-       // background.setBounds(0,0,50,50);
+        background.setIcon(temp1);
+        background.setBounds(0,0,864,614);
         container.add(background);
     }
     private void initialize_button(int i , int j){
@@ -82,23 +113,28 @@ public class ChessBoard extends JFrame{
         button.setDefaultCapable(false);
         button.setFocusPainted(false);
     }
-    private void action()
-    {
+    private void action() {
         button.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 JButton button = (JButton) e.getSource();
                 System.out.println(button.getName());
                 previous = current;
-                current = button.getName();
-                if(previous == null)return;
-                Icon temp = get_button(previous).getIcon();
-                if(temp != null)
+                current = ((JButton) e.getSource()).getName();
+                if(current == previous){
+                    previous = null ;
+                    return;
+                }
+                if(drag != null)
                 {
                     move_piece(previous,current);
-                    current = null;
                     previous = null;
+                    current = null;
+                    drag = null;
+                }
+                else if(get_button(current).getIcon() != null)
+                {
+                    drag = get_button(current).getIcon();
                 }
             }
         });
@@ -109,12 +145,10 @@ public class ChessBoard extends JFrame{
         result+= (pos.charAt(0)-'A');
         return result;
     }
-   private JButton get_button(String pos)
-   {
+   private JButton get_button(String pos) {
        return (JButton) ChessBoardPanel.getComponent(get_position(pos));
    }
-   private void move_piece(String p1 ,String p2)
-   {
+   private void move_piece(String p1 ,String p2) {
        Icon temp = get_button(p1).getIcon();
        if(temp == null)return;
        get_button(p1).setIcon(null);
--- src/MainPackage/PieceIcons.java
+++ src/MainPackage/PieceIcons.java
@@ -17,29 +17,29 @@ public class PieceIcons {
     public Image white_rook;
     public PieceIcons()
     {
-        ImageIcon temp1 = new ImageIcon("src/pieces/black_bishop.png");
-        black_bishop = temp1.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp2 = new ImageIcon("src/pieces/black_king.png");
-        black_king = temp2.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp3 = new ImageIcon("src/pieces/black_knight.png");
-        black_knight = temp3.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp4 = new ImageIcon("src/pieces/black_pawn.png");
-        black_pawn = temp4.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp5 = new ImageIcon("src/pieces/black_queen.png");
-        black_queen = temp5.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp6 = new ImageIcon("src/pieces/black_rook.png");
-        black_rook = temp6.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp11= new ImageIcon("src/pieces/white_bishop.png");
-        white_bishop = temp11.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp22= new ImageIcon("src/pieces/white_king.png");
-        white_king = temp22.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp33= new ImageIcon("src/pieces/white_knight.png");
-        white_knight = temp33.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp44= new ImageIcon("src/pieces/white_pawn.png");
-        white_pawn = temp44.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp55= new ImageIcon("src/pieces/white_queen.png");
-        white_queen = temp55.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
-        ImageIcon temp66= new ImageIcon("src/pieces/white_rook.png");
-        white_rook = temp66.getImage().getScaledInstance(60,60,Image.SCALE_SMOOTH);
+        ImageIcon temp1 = new ImageIcon("src/Mat/Comp/game/Black/bishop.png");
+        black_bishop = temp1.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp2 = new ImageIcon("src/Mat/Comp/game/Black/king.png");
+        black_king = temp2.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp3 = new ImageIcon("src/Mat/Comp/game/Black/knight.png");
+        black_knight = temp3.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp4 = new ImageIcon("src/Mat/Comp/game/Black/pawn.png");
+        black_pawn = temp4.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp5 = new ImageIcon("src/Mat/Comp/game/Black/queen.png");
+        black_queen = temp5.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp6 = new ImageIcon("src/Mat/Comp/game/Black/rook.png");
+        black_rook = temp6.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp11= new ImageIcon("src/Mat/Comp/game/White/bishop.png");
+        white_bishop = temp11.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp22= new ImageIcon("src/Mat/Comp/game/White/king.png");
+        white_king = temp22.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp33= new ImageIcon("src/Mat/Comp/game/White/knight.png");
+        white_knight = temp33.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp44= new ImageIcon("src/Mat/Comp/game/White/pawn.png");
+        white_pawn = temp44.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp55= new ImageIcon("src/Mat/Comp/game/White/queen.png");
+        white_queen = temp55.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
+        ImageIcon temp66= new ImageIcon("src/Mat/Comp/game/White/rook.png");
+        white_rook = temp66.getImage().getScaledInstance(40,50,Image.SCALE_SMOOTH);
     }
 }
