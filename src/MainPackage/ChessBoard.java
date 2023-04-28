783cd7861a705dfdc251d9e5476ebdd5d85f5338
salah mohamed <salah.ismael03@eng-st.cu.edu.eg>
2023-04-28 18:29:16 +0300

    ChessBoard update

--- src/MainPackage/ChessBoard.java
+++ src/MainPackage/ChessBoard.java
@@ -1,31 +1,30 @@
 package MainPackage;
 import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 
-public class ChessBoard extends JFrame{
-    public JFrame board;
+public class ChessBoard{
+    private JFrame board;
     private PieceIcons icon;
     public JButton button,resign,draw, resign2,draw2;
     public String current;
-    Icon drag;
+    public Icon drag;
     public String previous;
-    private boolean turn;
     private JPanel container;
     private JPanel ChessBoardPanel;
     public ChessBoard(){initialize();}
-    public void initialize() {
+    private void initialize() {
         icon = new PieceIcons();
-        turn = false;
         initialize_board();
         setButtons();
         draw_chessBoard();
         initializePieces();
-        //move_piece("E4","A8");
+        boardLight("E4",true);//test
         set_backgrounds();
         board.setVisible(true);// show the board
     }
-    public void initialize_board(){
+    private void initialize_board(){
         board = new JFrame();
         container = new JPanel(null);
         board.add(container);
@@ -35,7 +34,7 @@ public class ChessBoard extends JFrame{
         board.setLocationRelativeTo(null);
         board.setResizable(false);
     }
-    public void draw_chessBoard(){
+    private void draw_chessBoard(){
         ChessBoardPanel = new JPanel(new GridLayout(8 , 8 , 0 , 0));
         container.add(ChessBoardPanel);
         ChessBoardPanel.setBounds(70 ,70,478,478);
@@ -54,7 +53,7 @@ public class ChessBoard extends JFrame{
                 ChessBoardPanel.add(pos[i][j]);
             }
     }
-    public void initializePieces(){
+    private void initializePieces(){
         get_button("A2").setIcon(new ImageIcon(icon.white_pawn));
         get_button("B2").setIcon(new ImageIcon(icon.white_pawn));
         get_button("C2").setIcon(new ImageIcon(icon.white_pawn));
@@ -63,13 +62,43 @@ public class ChessBoard extends JFrame{
         get_button("F2").setIcon(new ImageIcon(icon.white_pawn));
         get_button("G2").setIcon(new ImageIcon(icon.white_pawn));
         get_button("H2").setIcon(new ImageIcon(icon.white_pawn));
+
+        get_button("A7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("B7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("C7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("D7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("E7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("F7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("G7").setIcon(new ImageIcon(icon.black_pawn));
+        get_button("H7").setIcon(new ImageIcon(icon.black_pawn));
+
+        get_button("A8").setIcon(new ImageIcon(icon.black_rock));
+        get_button("B8").setIcon(new ImageIcon(icon.black_knight));
+        get_button("C8").setIcon(new ImageIcon(icon.black_bishop));
+        get_button("D8").setIcon(new ImageIcon(icon.black_queen));
+        get_button("E8").setIcon(new ImageIcon(icon.black_king));
+        get_button("F8").setIcon(new ImageIcon(icon.black_bishop));
+        get_button("G8").setIcon(new ImageIcon(icon.black_knight));
+        get_button("H8").setIcon(new ImageIcon(icon.black_rock));
+
+        get_button("A1").setIcon(new ImageIcon(icon.white_rock));
+        get_button("B1").setIcon(new ImageIcon(icon.white_knight));
+        get_button("C1").setIcon(new ImageIcon(icon.white_bishop));
+        get_button("D1").setIcon(new ImageIcon(icon.white_queen));
+        get_button("E1").setIcon(new ImageIcon(icon.white_king));
+        get_button("F1").setIcon(new ImageIcon(icon.white_bishop));
+        get_button("G1").setIcon(new ImageIcon(icon.white_knight));
+        get_button("H1").setIcon(new ImageIcon(icon.white_rock));
     }
-    public JButton createButton(String name, String filename, int x , int y) {
+    private JButton createButton(String name,String ref, String filename, int x , int y,String color) {
         JButton temp = new JButton(name);
+        temp.setName(ref);
         temp.setFocusable(false);
-        temp.setFont(new Font("SpaceGrotesk",Font.PLAIN,20));
+        temp.setFont(new Font("Space Grotesk Light",Font.BOLD,20));
+        temp.setBackground(Color.red);
+        temp.setForeground(Color.decode(color));
         ImageIcon temp1 = new ImageIcon(filename);//src/Mat/Buttons/resignBtn.png
-        Image btnBG = temp1.getImage().getScaledInstance(66,30,Image.SCALE_SMOOTH);
+        Image btnBG = temp1.getImage().getScaledInstance(75,35,Image.SCALE_SMOOTH);
         temp.setIcon(new ImageIcon (btnBG));
         temp.setHorizontalTextPosition(JButton.CENTER);
         temp.setVerticalTextPosition(JButton.CENTER);
@@ -81,25 +110,22 @@ public class ChessBoard extends JFrame{
                 System.out.println("hello world");
             }
         });
-        temp.setBounds(x,y,66,30);
+        temp.setBounds(x,y,75,35);
         return temp;
     }
-    public void setButtons()
-    {
-        resign = createButton("resign","src/Mat/Buttons/drawBtn.png",640,250);
-        draw = createButton("draw","src/Mat/Buttons/resignBtn.png",730,250);
-        resign2 = createButton("resign2","src/Mat/Buttons/drawBtn.png",640,340);
-        draw2 = createButton("draw2","src/Mat/Buttons/resignBtn.png",730,340);
+    private void setButtons() {
+        resign = createButton("Resign","re1","src/Mat/Buttons/drawBtn.png",635,250,"#FF006E");
+        draw = createButton("Draw","dr1","src/Mat/Buttons/resignBtn.png",725,250,"#5F5F5F");//5F5F5F
+        resign2 = createButton("Resign","re2","src/Mat/Buttons/drawBtn.png",635,340,"#FF006E");
+        draw2 = createButton("Draw","dr2","src/Mat/Buttons/resignBtn.png",725,340,"#5F5F5F");
         container.add(resign);
         container.add(draw);
         container.add(resign2);
         container.add(draw2);
     }
     private void set_backgrounds() {
-       // ImageIcon backgroundImage = new  ImageIcon("src/Mat/BackG/gameFields.png");
-        Image backgroundImage;
+
         ImageIcon temp1 = new ImageIcon("src/Mat/BackG/rgameFields.png");
-       // backgroundImage = temp1.getImage().getScaledInstance(860,600,Image.SCALE_DEFAULT);
         JLabel background = new JLabel();
         background.setIcon(temp1);
         background.setBounds(0,0,864,614);
@@ -148,11 +174,37 @@ public class ChessBoard extends JFrame{
    private JButton get_button(String pos) {
        return (JButton) ChessBoardPanel.getComponent(get_position(pos));
    }
-   private void move_piece(String p1 ,String p2) {
+   public void move_piece(String p1 ,String p2) {
        Icon temp = get_button(p1).getIcon();
        if(temp == null)return;
        get_button(p1).setIcon(null);
        get_button(p2).setIcon(temp);
    }
+   public void boardLight(String pos , boolean flag){
+       boolean empty = get_button(pos).getIcon() == null;
+       ImageIcon temp1 = new ImageIcon("src/Mat/Comp/game/avMove.png");
+       Image av = temp1.getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+       ImageIcon temp2 = new ImageIcon("src/Mat/Comp/game/noMove.png");
+       Image no = temp2.getImage().getScaledInstance(30,30,Image.SCALE_SMOOTH);
+    if(flag){
+        if(empty)
+            get_button(pos).setIcon(new ImageIcon(av));
+        else
+        {
+            //
+        }
+    }
+    else {
+        if(empty)
+            get_button(pos).setIcon(null);
+        else{
+            //
+        }
 
+    }
+   }
+   public static void main(String []args)
+   {
+       ChessBoard c = new ChessBoard();
+   }
 }
