76feeeea1156408ba7b9a12dacb4587a200b92a5
salah mohamed <salah.ismael03@eng-st.cu.edu.eg>
2023-04-29 04:53:13 +0300

    user class

---
+++ src/MainPackage/User.java
@@ -0,0 +1,34 @@
+package MainPackage;
+
+import java.util.ArrayList;
+// عملتلك الحاجات الاساسية عشان تعرف تتخيل باقي الشغل
+public class User {
+    private String Username;
+    private String Password;
+    public double WinRate;
+    public ArrayList<Match> History;
+    // make the constructor to initialize the name and password
+    // make function to add match to the history
+    // function to calculate the win rate
+    // use json files to store the data for each instance from this class
+}
+class Match {
+    public User Player1;
+    public User Player2;
+    public ArrayList<Move> moves;
+    // make the constructor to initialize the players.
+    // make function to add move to the arraylist moves.
+    // arraylist moves is array to store the flow of the game like -  1.E4 , E5 - so that the fist one
+    // is the white move and the second is the black move.
+    // use json files to store the data for each instance from this class
+}
+class Move{
+    String WhiteMove;
+    String BlackMove;
+    public Move MakeMove(String white, String black) { // use this method to add moves to the array list moves
+        Move move = new Move();
+        move.WhiteMove = white;
+        move.BlackMove = black;
+        return move;
+    }
+}
